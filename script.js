18810e7e87070e4b590965a152acac4b7bcae5f3
KiptooRugut <mccrug97@gmail.com>
2025-05-12 00:45:17 +0300

    Script function to change the text content of the paragraph

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Function to change the text content of the paragraph
+function changeText() {
+    const textElement = document.getElementById('dynamicText');
+    textElement.textContent = "The text has been changed! 🎉";
+}
